6e20dbe8f9c11480f7815d950c0e8c1331fbd151
benevo <benevo@163.com>
2017-02-26 11:27:48 +0800

    main.cpp: add Q_OS_MAC to control qml file

--- main.cpp
+++ main.cpp
@@ -5,7 +5,19 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine(QUrl(QStringLiteral("../../main.qml")));
+    QQmlApplicationEngine engine;
+
+#ifdef Q_USE_QML_RC
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+#else
+    engine.load(QUrl::fromLocalFile(QGuiApplication::applicationDirPath() +
+                                #if defined(Q_OS_WIN)
+                                    QStringLiteral("")
+                                #elif defined(Q_OS_MAC)
+                                    QStringLiteral("/../../..")
+                                #endif
+                                + QStringLiteral("/../../main.qml")));
+#endif
 
     return app.exec();
 }
